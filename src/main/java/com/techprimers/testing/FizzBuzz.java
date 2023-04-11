24ef9557f9a30f98cf1e470dbf545c79ac92460a
VegiSujini <sujinivegi@gmail.com>
2023-04-11 06:28:33 +0000

    Hi

--- src/main/java/com/techprimers/testing/FizzBuzz.java
+++ src/main/java/com/techprimers/testing/FizzBuzz.java
@@ -1,9 +1,9 @@
-package com.techprimers.testing;
+﻿package com.techprimers.testing;
 
 public class FizzBuzz {
 
     public String play(int number) {
-	//saii3
+	//mam3
         if (number == 0) throw new IllegalArgumentException("Number must not be 0");
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
